23494a9e483ea7d1a8da8c261ce0af109f6a69d8
CalvinKarume <johncalvinkarume@gmail.com>
2023-12-04 14:49:29 -0800

    ANOTHER ERROR FIXEDDD

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,9 +1,13 @@
-export function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
-  }
-  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
-    throw new Error('Endpoint load is high');
-  }
-}
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   ../0x01-ES6_promise/3-all.js
+	modified:   ../0x02-ES6_classes/5-building.js
+	modified:   ../0x02-ES6_classes/6-sky_high.js
+	modified:   100-weak.js
+	modified:   6-set.js
+
+no changes added to commit (use "git add" and/or "git commit -a")
